d2eac2b5eadd7766017ca2bcadce6b847ab42c7a
k8k8812 <84696434+k8k8812@users.noreply.github.com>
2022-02-21 18:31:37 -0500

    add the main function

--- src/dollarsbankApplication/DollarsBankApplication.java
+++ src/dollarsbankApplication/DollarsBankApplication.java
@@ -1,4 +1,9 @@
 package dollarsbankApplication;
 
 public class DollarsBankApplication {
+
+    public static void main(String[] args){
+        System.out.println("hello java application!");
+
+    }
 }
