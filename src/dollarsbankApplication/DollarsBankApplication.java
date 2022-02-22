d0b7ed13ef083b07e332e48292daef0c25c69f83
k8k8812 <84696434+k8k8812@users.noreply.github.com>
2022-02-22 17:25:31 -0500

    moved codes from main;

--- src/dollarsbankApplication/DollarsBankApplication.java
+++ src/dollarsbankApplication/DollarsBankApplication.java
@@ -1,57 +1,17 @@
 package dollarsbankApplication;
-
-
-import java.util.Arrays;
-import java.util.Scanner;
-
+import dollarsBankConsoleView.LoginMenu;
 
 public class DollarsBankApplication {
 
     // Testing
     public static void main(String[] args) {
-        DollarsBankApplication dba = new DollarsBankApplication();
-        while(dba.login_page() != 3){
-            dba.login_page();
+        LoginMenu lm = new LoginMenu();
+
+        while(lm.login_page() != 3){
+            lm.login_page();
         }
-    }
-
-    public int login_page(){
-
-        //initialize menu
-        int[] login_menu_number = new int[]{1,2, 3};
-        String[] login_menu = new String[]{"Create New Account", "Login", "Exit"};
-
-        // set scanner to get user input;
-        Scanner scanner = new Scanner(System.in);
-
-        //print out the menu choices;
-        System.out.println("Welcome to DollarsBank! ");
-        for (int i = 0; i < login_menu_number.length; i++) {
-            System.out.println(login_menu_number[i] + ". " + login_menu[i] + ";");
-        }
-        System.out.println("Choose a number(1,2 or 3):" );
-
-
-        //get user's choice;
-        int chosen_number = scanner.nextInt();
-        //check if user's input is in the menu;
-        int result = Arrays.binarySearch(login_menu_number, chosen_number);
-        boolean check_number = result > -1 ? true: false;
-
-        if(check_number){
-            if(chosen_number == 3){
-                System.out.println("Thank you for choosing DollarsBank, goodbye!");
-                System.exit(0);
-            }
-            System.out.println("The number chosen was: " + chosen_number);
-
-        }else{
-            System.out.println("Sorry but the number you chose was not in the menu!");
-        }
-        return chosen_number;
     }
 }
-
 //        Customer c = new Customer("Stephen",
 //                "Renton",
 //                "505-0050",
