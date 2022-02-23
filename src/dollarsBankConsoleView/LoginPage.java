a60b513e125b86686849327af50f5f986d7a103a
k8k8812 <84696434+k8k8812@users.noreply.github.com>
2022-02-22 23:39:01 -0500

    added LoginPage.java;
    when user selected "1" from the welcome_page (LoginMenu.java); this is the page the user will get to.

---
+++ src/dollarsBankConsoleView/LoginPage.java
@@ -0,0 +1,36 @@
+package dollarsBankConsoleView;
+import java.awt.*;
+import java.util.Scanner;
+import dollarsbankController.DollarsBankController;
+
+public class LoginPage {
+
+    //show login page
+    public void login_page(){
+
+        Scanner scanner = new Scanner(System.in);
+        DollarsBankController dbc = new DollarsBankController();
+
+        // get the user id;
+        System.out.println("========================");
+        System.out.print("Enter your user id: ");
+        String id = scanner.nextLine();
+
+        // get the user password;
+        System.out.println("========================");
+        System.out.print("Enter your user password: ");
+        String password = scanner.nextLine();
+
+        // use controller to check if they're valid;
+        boolean result = dbc.login_check(id, password);
+
+        // if valid:
+        if(result){
+            //do something;
+
+        }else{
+            // do something;
+        }
+
+    }
+}
