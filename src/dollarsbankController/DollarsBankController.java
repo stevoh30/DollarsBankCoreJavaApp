404e773df376eea71e40e267882fc025be8ca8bb
k8k8812 <84696434+k8k8812@users.noreply.github.com>
2022-02-27 02:28:33 -0500

    updated

--- src/dollarsbankController/DollarsBankController.java
+++ src/dollarsbankController/DollarsBankController.java
@@ -10,10 +10,5 @@ public class DollarsBankController {
 //        this.customers = customers;
 //    }
 //
-    public boolean login_check(String id, String password){
-        // check if user id is existed;
-        boolean result = false;
-        System.out.println("validation codes to be implemented here");
-        return false;
-    }
+
 }
