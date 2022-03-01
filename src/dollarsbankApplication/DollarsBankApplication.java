941d3497bf752748ae00e514558232e8fb41c1c9
stevoh30 <36320944+stevoh30@users.noreply.github.com>
2022-02-28 20:16:57 -0800

    Saving Data Functionality
    Added functionality to the project to be able to save customers as a json file within the project.

--- src/dollarsbankApplication/DollarsBankApplication.java
+++ src/dollarsbankApplication/DollarsBankApplication.java
@@ -1,34 +1,37 @@
 package dollarsbankApplication;
 import dollarsBankConsoleView.LoginMenu;
 import dollarsbankModel.Customer;
+import dollarsbankModel.CustomerService;
 
+import java.io.IOException;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class DollarsBankApplication {
 
-    public static void main(String[] args) {
-        Test();
+    public static void main(String[] args) throws IOException {
+        CustomerService service = new CustomerService();
+        service.Test();
+        //Test();
         LoginMenu lm = new LoginMenu();
 
         while (lm.welcome_page() != 3) {
             lm.welcome_page();
         }
     }
-// <<<<<<< HEAD
+
 //     // Testing
 //     public static void Test(){
-// =======
+
 
 //     //Testing
 //     public static void Test() {
-// >>>>>>> 2c2b1eb1ecda8acde95f5bad807bdcd4b52038b2
+
 //         Customer c = new Customer("Stephen",
 //                 "Renton",
 //                 "505-0050",
 //                 "Stevoh",
 //                 "Password",
-// <<<<<<< HEAD
 //                 500, new PriorityQueue<String>());
 
 //         c.deposit(250);
@@ -39,7 +42,7 @@ public class DollarsBankApplication {
 //     }
 
 // =======
-//                 500, new PriorityQueue<>());
+//           500, new PriorityQueue<>());
 
 // //        c.deposit(250);
 // //        c.withdraw(10000);
@@ -47,6 +50,5 @@ public class DollarsBankApplication {
 // //        c.balanceHistory();
 // //        c.ToString();
 //     }
-// >>>>>>> 2c2b1eb1ecda8acde95f5bad807bdcd4b52038b2
 }
 
---
+++ src/dollarsbankModel/CustomerService.java
@@ -0,0 +1,74 @@
+package dollarsbankModel;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.*;
+
+import java.io.FileWriter;
+
+
+public class CustomerService {
+
+    //private fields
+    //Customer customers[] = new Customer[]{};
+    public List<Customer> customers = new ArrayList<>();
+
+    //functionality
+    //write customer information to json file
+
+    public void Test() throws IOException {
+
+//        Map<String, String> colours = new HashMap<>();
+//        colours.put("BLACK", "#000000");
+//        colours.put("RED", "#FF0000");
+//        colours.put("GREEN", "#008000");
+//        colours.put("BLUE", "#0000FF");
+//        colours.put("YELLOW", "#FFFF00");
+//        colours.put("WHITE", "#FFFFFF");
+//
+//        // Convert a Map into JSON string.
+//        Gson gson = new Gson();
+//        String json = gson.toJson(colours);
+//        System.out.println("json = " + json);
+//
+//        // Convert JSON string back to Map.
+//        Type type = new TypeToken<Map<String, String>>() {
+//        }.getType();
+//        Map<String, String> map = gson.fromJson(json, type);
+//        for (String key : map.keySet()) {
+//            System.out.println("map.get = " + map.get(key));
+//        }
+
+        // Converts ArrayList into Json Object
+        Customer c = new Customer("Stephen",
+                 "Renton",
+                 "505-0050",
+                 "Stevoh",
+                 "Password",
+                 500, new PriorityQueue<String>());
+        customers.add(c);
+        customers.add(c);
+
+        var gson = new Gson();
+        String jsonCustomers = gson.toJson(customers);
+        System.out.println("jsonCustomers = " + jsonCustomers);
+
+        //save Json Objects to file
+        FileWriter writer = new FileWriter("data.json");
+        gson.toJson(customers, writer);
+        writer.flush();
+        writer.close();
+
+        // Converts JSON string into a collection of Customer object.
+        Type type = new TypeToken<List<Customer>>() {
+        }.getType();
+        List<Customer> customerList = gson.fromJson(jsonCustomers, type);
+
+        for (Customer customer : customerList) {
+            customer.ToString();
+        }
+    }
+}
